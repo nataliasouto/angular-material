766ea01b68eead8d8475913923eeae56e688c641
Natalia Souto <nataliasoutoo@gmail.com>
2021-02-01 11:11:39 -0300

    create operations of the crud

--- src/app/service/meeting.service.ts
+++ src/app/service/meeting.service.ts
@@ -37,8 +37,26 @@ export class MeetingService {
     return this.http.put(this.MOBILE_BAAS_URL+'?table='+this.tableName,meeting,this.httpOptions)
   }
 
-  getAll(){
+  getAll(pageNumber:number, totalRecordsPerPage: number, sortField:string, filters:string){
+    let parameters = '?table='+this.tableName;
 
+    if(pageNumber != null){
+      parameters += '&pageNumber='+pageNumber;
+    }
+
+    if(totalRecordsPerPage != null){
+      parameters += '&totalRecordsPerPage='+totalRecordsPerPage;
+    }
+
+    if(sortField != null){
+      parameters += '&sortField='+sortField;
+    }
+
+    if(filters != null){
+      parameters += '&filters='+filters;
+    }
+
+    return this.http.get(this.MOBILE_BAAS_URL+'/find'+parameters,this.httpOptions);
   }
 
 }
