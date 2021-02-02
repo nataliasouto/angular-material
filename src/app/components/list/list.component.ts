6024391a18301fdfc7200cc7e1054d6ede089187
Natalia Souto <nataliasoutoo@gmail.com>
2021-02-02 16:20:03 -0300

    integração com o mobile

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,24 +1,8 @@
+import { FormComponent } from './../form/form.component';
 import { Component, OnInit } from '@angular/core';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { MeetingService } from 'src/app/service/meeting.service';
+import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 
 
 @Component({
@@ -27,12 +11,33 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-
-  constructor() { }
+  displayedColumns: string[] = ['formName', 'formSubject', 'formResponsible'];
+  meetings = [];
+  length: number;
+  totalRecordsPerPage: number = 5;
+  meetingNameFind: string;
+  meetingDateFind: string;
+  
+  constructor(
+    private service: MeetingService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.findAll(0,'formDate',null);
   }
 
+
+  findAll(pageNumber:number, sortField:string, filters:string){
+    this.service.getAll(pageNumber,this.totalRecordsPerPage,sortField,filters).subscribe(meetingsReturn => {
+          this.meetings =  meetingsReturn['meeting'];
+          this.length = meetingsReturn['page'].size;
+      }, error => {
+        this.meetings = [];
+        console.log('Error ',error);
+        console.log('Error status ',error.status);
+        console.log('Error error ',error.error);
+        console.log('Error headers ',error.headers);
+      });
+  }
 }
